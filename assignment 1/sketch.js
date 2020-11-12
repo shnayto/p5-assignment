0a715b19974b4dfed6069a3b429866209acdda05
shnayto <72550336+shnayto@users.noreply.github.com>
2020-11-12 12:04:55 +0000

    Update sketch.js
    fixed the splicing from array issue
    added a second sound loop

--- assignment 1/sketch.js
+++ assignment 1/sketch.js
@@ -5,17 +5,13 @@ let oscSine2;
 let oscSine3;
 // let midiNotes = [82, 84, 92, 122]; eventual midi note to freq
 // let noteIndex = 0;
-let playing = false;
-let playing0 = false;
-let playing1 = false;
-let playing2 = false;
 let counts = []; // array counting the number of playing oscillators
-let stop;
 
 function setup() {
   createCanvas(800, 600);
 
-  soundLoop = new p5.SoundLoop(onSoundLoop, 1);  // 1 second soundloop
+  soundLoop1 = new p5.SoundLoop(onSoundLoop1, 1);  // 1 second soundloop
+  soundLoop2 = new p5.SoundLoop(onSoundLoop2, 0.1);  // 0.1 second soundloop
 
   oscSine = new p5.Oscillator('sine');  // circle synth 1
   oscSine2 = new p5.Oscillator('sine');
@@ -40,21 +36,43 @@ function setup() {
 }
 
 
-function onSoundLoop() {
+function onSoundLoop1() {
   for (let i = 0; i < circles.length; i++)
-      if (counts.includes(0)) {
+      if (counts.includes(0) && circles[0].x < width/2) {
       oscSine.start();
       oscSine.amp(0.2);
       oscSine.amp(0, 0.5);
       oscSine.freq(random(100, 200));
     }
-    if (counts.includes(1)) {
+    if (counts.includes(1) && circles[1].x < width/2) {
       oscSine2.start();
       oscSine2.amp(0.2);
       oscSine2.amp(0, 0.5);
       oscSine2.freq(random(300, 301));
     }
-    if (counts.includes(2)) {
+    if (counts.includes(2) && circles[2].x < width/2) {
+      oscSine3.start();
+      oscSine3.amp(0.2);
+      oscSine3.amp(0, 0.5);
+      oscSine3.freq(random(1000, 1001));
+    }
+}
+
+function onSoundLoop2() {
+  for (let i = 0; i < circles.length; i++)
+      if (counts.includes(0) && circles[0].x > width/2) {
+      oscSine.start();
+      oscSine.amp(0.2);
+      oscSine.amp(0, 0.5);
+      oscSine.freq(random(100, 200));
+    }
+    if (counts.includes(1) && circles[1].x > width/2) {
+      oscSine2.start();
+      oscSine2.amp(0.2);
+      oscSine2.amp(0, 0.5);
+      oscSine2.freq(random(300, 301));
+    }
+    if (counts.includes(2) && circles[2].x > width/2) {
       oscSine3.start();
       oscSine3.amp(0.2);
       oscSine3.amp(0, 0.5);
@@ -64,33 +82,27 @@ function onSoundLoop() {
 
 function mousePressed() {
   for (let i = 0; i < circles.length; i++)
-      if (circles[i].clicked() && counts.length == (i + 1)) {
-        counts.splice(i);
-        console.log(counts);
-        soundLoop.start();
-      } else if (circles[i].clicked() && counts.length < (i + 3)) {
+      if (circles[i].clicked() && counts.indexOf(i) !== -1){ // if circle x is clicked and x is within array
+        counts.splice(counts.indexOf(i), 1);                 //splice x from array
+      } else if (circles[i].x > width/2 && circles[i].clicked() && counts.includes(i) == false){
         counts.push(i);
-        console.log(counts);
-        soundLoop.start();
+        soundLoop2.start();
+      } else if (circles[i].x < width/2 && circles[i].clicked() && counts.includes(i) == false){
+        counts.push(i);
+        soundLoop1.start();
       }
-    //   } else if (circles[i].clicked() && playing == true){
-    //     count.splice(i);
-    //     console.log(playing);
-    // }
 }
 
-// function mouseDragged() {
-//     for (let i = 0; i < circles.length; i++){
-//       let offset = dist(mouseX, mouseY, circles[i].x, circles[i].y);
-//       if (circles[i].rollover() == true) {
-//         circles[i].x = mouseX
-//         circles[i].y = mouseY
-//         soundLoop.stop();
-//         console.log('stop');
-//         playing = false;
-//       }
-//     }
-// }
+function mouseDragged() {
+    for (let i = 0; i < circles.length; i++){
+      let offset = dist(mouseX, mouseY, circles[i].x, circles[i].y);
+      if (circles[i].rollover() == true) {
+        circles[i].x = mouseX
+        circles[i].y = mouseY
+        // soundLoop1.stop();
+      }
+    }
+}
 
 function draw() {
   background(35);
